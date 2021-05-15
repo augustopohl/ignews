29dddb71632041552be339f81cb3dbfb53a48212
Augusto Pohl <augustopohl@hotmail.com>
2021-05-15 17:52:43 -0300

    authorization

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,6 +6,6 @@ let cookies = parseCookies();
 export const api = axios.create({
     baseURL: `${process.env.NEXTAUTH_URL}/api`,
     headers: {
-        Authorization: `Bearer ${cookies['nextauth.token']}`,
+        Authorization: `Bearer ${process.env.STRIPE_API_KEY}`,
     }
 })
