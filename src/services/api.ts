9a8c3463db4f8ee729b410b8734f4b8bf3e0d321
Augusto Pohl <augustopohl@hotmail.com>
2021-07-03 20:45:08 -0300

    ready

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,11 @@
 import axios from 'axios'
+import { parseCookies } from 'nookies';
+
+let cookies = parseCookies();
 
 export const api = axios.create({
-    baseURL: '/api',
+    baseURL: `${process.env.NEXTAUTH_URL}/api`,
+    headers: {
+        Authorization: `Bearer ${cookies['nextauth.token']}`,
+    }
 })
